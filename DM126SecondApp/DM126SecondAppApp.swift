a46ab59f01baf335b680391c6df9133f4c4d08fb
Ana Monti <anajuliamonti8@gmail.com>
2024-11-09 11:26:15 -0500

    Initial Commit

---
+++ DM126SecondApp/DM126SecondAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DM126SecondAppApp.swift
+//  DM126SecondApp
+//
+//  Created by user270466 on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct DM126SecondAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DM126SecondAppTests/DM126SecondAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DM126SecondAppTests.swift
+//  DM126SecondAppTests
+//
+//  Created by user270466 on 11/9/24.
+//
+
+import Testing
+@testable import DM126SecondApp
+
+struct DM126SecondAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
